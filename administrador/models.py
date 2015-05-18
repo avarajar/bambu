ea80a1ed1ebf497aefdfbd080be6c1588cdc16ed
andrade <nirvajara@hotmail.com>
2015-05-17 22:00:12 -0500

    add unicode

--- administrador/models.py
+++ administrador/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 
+
 class Colore(models.Model):
-	color = models.CharField(max_length=15)
+    color = models.CharField(max_length=15)
+
+    def __unicode__(self):
+        return u"%s" % self.color
+
 
 class Categoria(models.Model):
-	categoria = models.CharField(max_length=20)
+    categoria = models.CharField(max_length=20)
+
+    def __unicode__(self):
+        return u"%s" % self.categoria
+
 
 class Producto(models.Model):
-	nombre = models.CharField(max_length=20)
+    nombre = models.CharField(max_length=20)
 
+    def __unicode__(self):
+        return u"%s" % self.nombre
